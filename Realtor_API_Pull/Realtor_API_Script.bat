fbbcd080840f8a2c1be34044f38c12735f33229a
peaceful-town <paxtondfreeman@gmail.com>
2021-04-26 16:01:28 -0500

    Create Realtor_API_Script.bat

---
+++ Realtor_API_Pull/Realtor_API_Script.bat
@@ -0,0 +1,3 @@
+"C:\Users\REMOVED\AppData\Local\Microsoft\WindowsApps\python.exe" "C:\Users\REMOVED\Documents\A Home for Us\Realtor API\Realtor API Pull.py
+"C:\Users\REMOVED\AppData\Local\Microsoft\WindowsApps\python.exe" "C:\Users\REMOVED\Documents\A Home for Us\Realtor API\Email Real Estate data.py
+pause
